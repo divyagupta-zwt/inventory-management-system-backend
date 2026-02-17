0e890b78f62798a7f6170f3c728a9083bced4e22
divyagupta-zwt <divya.gupta@zealousweb.com>
2026-02-17 11:06:39 -0800

    changes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express= require('express');
 const app= express();
+const cors= require('cors');
 const orderRoutes= require('./routes/order-routes');
 const warehouseRoutes= require('./routes/warehouse-routes')
 const productRoutes= require('./routes/product-routes')
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/orders', orderRoutes);
 app.use('/api/warehouse', warehouseRoutes);
 app.use('/api/products', productRoutes);
