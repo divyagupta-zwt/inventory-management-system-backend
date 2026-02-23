bc9e82b410606d915eef561fec97a0e2ea2c9bc4
divyagupta-zwt <divya.gupta@zealousweb.com>
2026-02-23 10:39:40 -0800

    changes

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -2,7 +2,7 @@ const db= require('../config/db');
 
 exports.getProducts= async(req, res)=>{
     try{
-        const [rows]=await db.query(`select product_name, sku, price from products;`);
+        const [rows]=await db.query(`select product_id, product_name, sku, price from products;`);
         res.json(rows);
     }catch(error){
         res.status(400).json({error: error.message});
